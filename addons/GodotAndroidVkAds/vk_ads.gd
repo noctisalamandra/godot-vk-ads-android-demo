9bbdf977256933ceddec4589f58011bc61905e97
Yoshi <dleonavichus@mail.ru>
2025-09-25 14:41:09 +0300

    Godot 4.5

--- addons/GodotAndroidVkAds/vk_ads.gd
+++ addons/GodotAndroidVkAds/vk_ads.gd
@@ -19,15 +19,15 @@ signal rewarded_video_failed_to_load(error_code)
 signal rewarded_video_closed
 
 # Properties
-@export var banner_id:int
-@export var banner_on_top:bool = false
-@export var interstitial_id:int
-@export var rewarded_id:int
+@export var banner_id: int
+@export var banner_on_top: bool = false
+@export var interstitial_id: int
+@export var rewarded_id: int
 
 # "Private" properties
 var _vk_singleton = null
-var _is_interstitial_loaded:bool = false
-var _is_rewarded_video_loaded:bool = false
+var _is_interstitial_loaded: bool = false
+var _is_rewarded_video_loaded: bool = false
 
 func _enter_tree():
 	if not init():
@@ -35,26 +35,31 @@ func _enter_tree():
 
 # Initialization
 func init() -> bool:
-	if(Engine.has_singleton("GodotAndroidVkAds")):
+	if Engine.has_singleton("GodotAndroidVkAds"):
 		_vk_singleton = Engine.get_singleton("GodotAndroidVkAds")
-		if not _vk_singleton.is_connected("_on_banner_loaded", _on_banner_loaded):
+		var banner_callable = Callable(self, "_on_banner_loaded")
+		if not _vk_singleton.is_connected("_on_banner_loaded", banner_callable):
 			connect_signals()
-			_vk_singleton.init()
-			return true
+		_vk_singleton.init()
+		return true
 	return false
 
 # Connect the VkAds Java signals
 func connect_signals() -> void:
-	# Banner
+	connect_banner_signals()
+	connect_interstitial_signals()
+	connect_rewarded_signals()
+
+func connect_banner_signals() -> void:
 	_vk_singleton._on_banner_loaded.connect(_on_banner_loaded)
 	_vk_singleton._on_banner_failed_to_load.connect(_on_banner_failed_to_load)
 
-	# Interstitial
+func connect_interstitial_signals() -> void:
 	_vk_singleton._on_interstitial_loaded.connect(_on_interstitial_loaded)
 	_vk_singleton._on_interstitial_failed_to_load.connect(_on_interstitial_failed_to_load)
 	_vk_singleton._on_interstitial_ad_dismissed.connect(_on_interstitial_ad_dismissed)
 
-	# Rewarded
+func connect_rewarded_signals() -> void:
 	_vk_singleton._on_rewarded.connect(_on_rewarded)
 	_vk_singleton._on_rewarded_video_ad_loaded.connect(_on_rewarded_video_ad_loaded)
 	_vk_singleton._on_rewarded_video_ad_failed_to_load.connect(_on_rewarded_video_ad_failed_to_load)
@@ -62,49 +67,45 @@ func connect_signals() -> void:
 
 # Load
 func load_banner() -> void:
-	if _vk_singleton != null:
+	if _vk_singleton:
 		_vk_singleton.loadBanner(banner_id, banner_on_top)
 
 func load_interstitial() -> void:
-	if _vk_singleton != null:
+	if _vk_singleton:
 		_vk_singleton.loadInterstitial(interstitial_id)
 
 func is_interstitial_loaded() -> bool:
-	if _vk_singleton != null:
-		return _is_interstitial_loaded
-	return false
+	return _is_interstitial_loaded
 
 func load_rewarded_video() -> void:
-	if _vk_singleton != null:
+	if _vk_singleton:
 		_vk_singleton.loadRewardedVideo(rewarded_id)
 
 func is_rewarded_video_loaded() -> bool:
-	if _vk_singleton != null:
-		return _is_rewarded_video_loaded
-	return false
+	return _is_rewarded_video_loaded
 
 # Show / hide
 func show_banner() -> void:
-	if _vk_singleton != null:
+	if _vk_singleton:
 		_vk_singleton.showBanner()
 
 func hide_banner() -> void:
-	if _vk_singleton != null:
+	if _vk_singleton:
 		_vk_singleton.hideBanner()
 
 func show_interstitial() -> void:
-	if _vk_singleton != null:
+	if _vk_singleton:
 		_vk_singleton.showInterstitial()
 		_is_interstitial_loaded = false
 
 func show_rewarded_video() -> void:
-	if _vk_singleton != null:
+	if _vk_singleton:
 		_vk_singleton.showRewardedVideo()
 		_is_rewarded_video_loaded = false
 
 # Dimension
 func get_banner_dimension() -> Vector2:
-	if _vk_singleton != null:
+	if _vk_singleton:
 		return Vector2(_vk_singleton.getBannerWidth(), _vk_singleton.getBannerHeight())
 	return Vector2()
 
@@ -113,7 +114,7 @@ func get_banner_dimension() -> Vector2:
 func _on_banner_loaded() -> void:
 	emit_signal("banner_loaded")
 
-func _on_banner_failed_to_load(error_code:int) -> void:
+func _on_banner_failed_to_load(error_code: int) -> void:
 	emit_signal("banner_failed_to_load", error_code)
 
 # Interstitial
@@ -121,7 +122,7 @@ func _on_interstitial_loaded() -> void:
 	_is_interstitial_loaded = true
 	emit_signal("interstitial_loaded")
 
-func _on_interstitial_failed_to_load(error_code:int) -> void:
+func _on_interstitial_failed_to_load(error_code: int) -> void:
 	_is_interstitial_loaded = false
 	emit_signal("interstitial_failed_to_load", error_code)
 
@@ -129,14 +130,14 @@ func _on_interstitial_ad_dismissed() -> void:
 	emit_signal("interstitial_closed")
 
 # Rewarded
-func _on_rewarded(type:String) -> void:
+func _on_rewarded(type: String) -> void:
 	emit_signal("rewarded", type)
 
 func _on_rewarded_video_ad_loaded() -> void:
 	_is_rewarded_video_loaded = true
 	emit_signal("rewarded_video_loaded")
 
-func _on_rewarded_video_ad_failed_to_load(error_code:int) -> void:
+func _on_rewarded_video_ad_failed_to_load(error_code: int) -> void:
 	_is_rewarded_video_loaded = false
 	emit_signal("rewarded_video_failed_to_load", error_code)
 
